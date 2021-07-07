104b8554e61834fc4cb3fd40477d2d2fc18e4d29
lys0l <dali9115@colorado.edu>
2021-07-06 18:12:59 -0600

    partials and navbar

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
+  .use(express.static(path.join(__dirname, '/resources')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+// Routes
+
