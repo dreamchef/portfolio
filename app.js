90d23eb470d0f31d2306978a0b4ecfddcf5bd41b
lys0l <dali9115@colorado.edu>
2021-07-10 16:09:52 -0600

    add music view, pass filenames

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
+const app = express();
+const fs = require('fs');
 
-express()
-  .use(express.static(path.join(__dirname, '/resources')))
+app
+  .use(express.static(path.join(__dirname)))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
+
+app.get('/', (req, res) => res.render('pages/index'));
+
+app.get('/music', (req, res) => {
+  contentDir = path.join(__dirname, 'content/music/');
+  var fileNames = fs.readdirSync(contentDir).filter(el => path.extname(el) === '.mp3');
+  res.render('pages/music', {
+    fileNames: fileNames
+  });
+});
 // Routes
 
