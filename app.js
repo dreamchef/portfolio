ad8c8142f0b933d583e281d7bd93a93b620681ae
lys0l <dali9115@colorado.edu>
2021-07-25 18:45:55 -0600

    guideware video feed

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 const app = express();
 const fs = require('fs');
+const https = require("https");
+const axios = require('axios');
 
 app
   .use(express.static(path.join(__dirname)))
@@ -13,7 +15,24 @@ app
 
 app.get('/', (req, res) => res.render('pages/index'));
 
-app.get('/video', (req, res) => res.render('pages/video'));
+app.get('/video', (req, res) => {
+  var apiKey = "AIzaSyDBI4bXfbhdwQTe4as5g7kPCGCNMCY2-b0";
+  var channelId = "UCrN1yxZoYwAlkzKSFysRMLw";
+  var youtubeQuery = "https://www.googleapis.com/youtube/v3/search?key=" + apiKey + 
+    "&channelId=" + channelId + "&part=id&order=date&maxResults=20";
+  axios({
+    url: youtubeQuery,
+      method: 'GET',
+      dataType:'json',
+    })
+      .then(apiRes => {
+        console.log(apiRes.data.items);
+        res.render('pages/video', {
+          videos: apiRes.data.items,
+          numVideos: apiRes.data.items.length
+        });
+      })
+});
 
 app.get('/music', (req, res) => {
   contentDir = path.join(__dirname, 'content/music/');
